893a9f4ccfbffe2ca02ab22d831e9759107973c3
Chris Fei <cfei@chrismint.(none)>
2012-11-13 23:56:07 -0500

    added stuff

--- web.rb
+++ web.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require 'typhoeus'
 require 'nokogiri'
 require_relative './rgapi.rb'
-require_realtive './connector.rb'
+require_relative './connector.rb'
 
 get '/' do 
   "Hello world!"
