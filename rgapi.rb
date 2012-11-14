692b735f03089e3ee56a3ed58b3a5dc48e37e2ba
Chris Fei <cfei@chrismint.(none)>
2012-11-14 00:14:13 -0500

    added stuff

--- rgapi.rb
+++ rgapi.rb
@@ -11,6 +11,10 @@ class RGAPI
     html_source = @conn.get_html 'http://rapgenius.com'
     html_doc = Nokogiri::HTML(html_source)
 
-    html_doc.css('.popular li a')
+    popular_links = html_doc.css('.popular li a')
+
+    for link in popular_links
+      puts link + "<br/>"
+    end
   end
 end
